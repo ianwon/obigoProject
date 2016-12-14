22096bbd0f41cdfa015870e96ef276ce51714388
YunSeongHyeon <shyeon1212@naver.com>
2016-12-14 14:31:09 +0900

    gggg
    gggg

---
+++ src/com/obigo/obigoproject/androiduservehicle/dao/AndroidUserVehicleDaoImpl.java
@@ -0,0 +1,21 @@
+package com.obigo.obigoproject.androiduservehicle.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.obigo.obigoproject.vo.AndroidUserVehicleVO;
+
+@Repository("androiduservehicleDao")
+public class AndroidUserVehicleDaoImpl implements AndroidUserVehicleDao {
+
+	@Autowired
+	SqlSession sqlSession;
+	@Override
+	public List<AndroidUserVehicleVO> getAndroidUserVehicleListByUserid(String userId) {
+		return sqlSession.selectList("obigoproject.AndroidUserVehicle.selectAndroidUserVehicle", userId);
+	}
+
+}
---
+++ src/com/obigo/obigoproject/androiduservehicle/service/AndroidUserVehicleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.obigo.obigoproject.androiduservehicle.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.obigo.obigoproject.androiduservehicle.dao.AndroidUserVehicleDao;
+import com.obigo.obigoproject.vo.AndroidUserVehicleVO;
+
+@Service("AndroiduservehicleService")
+public class AndroidUserVehicleServiceImpl implements AndroidUserVehicleService {
+	@Autowired
+	AndroidUserVehicleDao androiduservehicleDao;
+
+	@Override
+	public List<AndroidUserVehicleVO> getAndroidUserVehicleListByUserid(String userId) {
+		return androiduservehicleDao.getAndroidUserVehicleListByUserid(userId);
+	}
+
+}
