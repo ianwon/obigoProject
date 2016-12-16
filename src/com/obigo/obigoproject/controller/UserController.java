74d00262ab7ba7155e7eee7c7bc2717fb3083005
YunSeongHyeon <shyeon1212@naver.com>
2016-12-16 16:33:44 +0900

    1234
    1234

--- src/com/obigo/obigoproject/controller/UserController.java
+++ src/com/obigo/obigoproject/controller/UserController.java
@@ -4,13 +4,11 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.itextpdf.text.log.SysoCounter;
 import com.obigo.obigoproject.user.service.UserService;
 import com.obigo.obigoproject.userrequest.service.UserRequestService;
 import com.obigo.obigoproject.uservehicle.service.UserVehicleService;
