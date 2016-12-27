ad0c2f36ab065deee7952aaafec0de417ae5657f
kimkyungwoo <inthelord@hanmail.net>
2016-12-27 11:04:41 +0900

    aaa
    aa

--- src/com/obigo/obigoproject/user/dao/UserDao.java
+++ src/com/obigo/obigoproject/user/dao/UserDao.java
@@ -16,6 +16,9 @@ public interface UserDao {
 
 	public List<UsersVO> getAdminList();
 
+	// ID 검색을 통한 Login 통계를 출력할 때 대상에 해당하는 User ID의 목록을 출력하기 위함
+	public List<UsersVO> getLoginUserList(String userId);
+
 	// 해당 유저의 USERVEHICLE 찾기위함
 	public UsersVO getUser(String userId);
 
@@ -24,6 +27,6 @@ public interface UserDao {
 
 	// VEHICLE_NAME에 따른 PUSH메시지를 전송하기 위해 REGISTRATION_ID를 추출
 	public List<String> getRegistrationByModelCode(String modelCode);
-	
+
 	public int getUserCount();
 }
