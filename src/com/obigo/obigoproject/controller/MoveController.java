d49b1ba67f81fd062ffb990482f6ec730692937c
kimkyungwoo <inthelord@hanmail.net>
2017-01-04 14:01:29 +0900

    user request
    user request

--- src/com/obigo/obigoproject/controller/MoveController.java
+++ src/com/obigo/obigoproject/controller/MoveController.java
@@ -2,7 +2,9 @@ package com.obigo.obigoproject.controller;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -141,8 +143,19 @@ public class MoveController {
 	@RequestMapping("/userrequest")
 	public String moveUserRequest(Model model) {
 
-		List<UserRequestVO> list = userRequestService.getUserRequestList();
-		model.addAttribute("userRequestList", list);
+		List<UserRequestVO> userRequestList = userRequestService.getUserRequestList();
+		
+		List<VehicleVO> vehicleList = vehicleService.getVehicleList();
+		Map<String, String>vehicleMap=new HashMap<>();
+
+		for(VehicleVO vo:vehicleList){
+			vehicleMap.put(vo.getModelCode(),vo.getModelName());
+		}
+		
+		model.addAttribute("userRequestList", userRequestList);
+		model.addAttribute("vehicleMap", vehicleMap);
+		
+		
 		return "/jsp/userrequest";
 	}
 
